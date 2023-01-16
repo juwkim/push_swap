bd3f2225965f8eab4559d2dc1851165b96bd4f34
= <=>
2023-01-16 12:16:02 +0900

    Feat: overflow func. edit

--- includes/parse.h
+++ includes/parse.h
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/14 02:51:05 by juwkim            #+#    #+#             */
-/*   Updated: 2023/01/16 11:22:50 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/01/16 12:13:54 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,14 @@
 # include "deque.h"
 # include "binary_search_tree.h"
 
+# include <stdbool.h>
+
 typedef enum s_sign
 {
 	PLUS,
 	MINUS
 }	t_sign;
 
-void		parse(t_deque *a, const int argc, const char **argv);
+void	parse(t_deque *a, const int argc, const char **argv);
 
 #endif // PARSE_H
--- sources/parse.c
+++ sources/parse.c
@@ -6,14 +6,15 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:31:58 by juwkim            #+#    #+#             */
-/*   Updated: 2023/01/16 11:27:39 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/01/16 12:13:31 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parse.h"
 
 static int	atoi_and_check_overflow(const char *str);
-static bool	is_overflow(const t_sign sign, const int prev, const int num);
+static bool	is_mul_overflow(const int a, const int b);
+static bool	is_add_overflow(const int a, const int b);
 static bool	is_duplication(const t_deque *a);
 
 void	parse(t_deque *a, const int argc, const char **argv)
@@ -45,7 +46,6 @@ void	parse(t_deque *a, const int argc, const char **argv)
 static int	atoi_and_check_overflow(const char *str)
 {
 	int		num;
-	int		prev;
 	t_sign	sign;
 
 	sign = PLUS;
@@ -57,23 +57,33 @@ static int	atoi_and_check_overflow(const char *str)
 	}
 	if (ft_isdigit(*str) == false)
 		ft_error_and_exit("space in front of digit while parsing integer");
-	if (sign == PLUS)
-		num = *str++ - '0';
-	else
-		num = - (*str++ - '0');
+	num = 0;
 	while (ft_isdigit(*str))
 	{
-		prev = num;
-		num = num * 10 + *str++ - '0';
-		if (is_overflow(sign, prev, num))
+		if (is_mul_overflow(num, 10))
 			ft_error_and_exit("size of integer is so big");
+		if (is_add_overflow(num * 10, *str - '0'))
+			ft_error_and_exit("size of integer is so big");
+		if (sign == PLUS)
+			num = num * 10 + (*str++ - '0');
+		else
+			num = num * 10 - (*str++ - '0');
 	}
 	return (num);
 }
 
-static bool	is_overflow(const t_sign sign, const int prev, const int num)
+static bool	is_mul_overflow(const int a, const int b)
 {
-	return ((sign == PLUS && num < prev) || (sign == MINUS && num > prev));
+	const int	result = a * b;
+
+	return (result / a != b);
+}
+
+static bool	is_add_overflow(const int a, const int b)
+{
+	const int	result = a + b;
+
+	return ((a > 0 && b > 0 && result < 0) || (a < 0 && b < 0 && result >= 0));
 }
 
 static bool	is_duplication(const t_deque *a)
