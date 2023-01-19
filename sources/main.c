e6903842d3a9624937e9e49da8d61c6575fea157
= <=>
2023-01-19 20:34:58 +0900

    norm success

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: juwkim <juwkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 12:31:40 by juwkim            #+#    #+#             */
-/*   Updated: 2023/01/19 20:30:42 by juwkim           ###   ########.fr       */
+/*   Updated: 2023/01/19 20:34:34 by juwkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,9 @@ int	main(int argc, char **argv)
 	dq_init(&ps.b);
 	list_init(&ps.res);
 	parse(&ps.a, argc, (const char **) argv);
-	dq_print(&ps.a);
-	dq_print(&ps.b);
 	if (dq_size(&ps.a) == 3)
 		simple_case(&ps);
 	else
 		a_to_b(&ps, dq_size(&ps.a));
 	list_print(&ps.res);
-	dq_print(&ps.a);
-	dq_print(&ps.b);
 }
-
